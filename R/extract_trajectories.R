169292d067b4238d296fd95c9e9db8058a130e2a
R. Scherrer <p278834@WORKSPACE.RUG.NL>
2018-12-12 11:12:44 +0100

    new function to extract

--- R/extract_trajectories.R
+++ R/extract_trajectories.R
@@ -1,46 +1,100 @@
 #' Extract trajectories in speciation cube
 #'
-#' @param dir A string. The path to the folders containing the simulations.
-#' @param is.complete A data frame. Contains information about habitat asymmetry, selection coefficient and whether speciation was complete or not (see argument `ri.threshold`) for each simulation. Typically the output of `find_completed`.
-#' @return A list of data frames, one for each simulation. Each data frame contains three columns, one for each dimension of the speciation cube.
+#' @param datapath Path to where the simulation folders are.
+#' @param outpath Where to save the output data frame.
+#' @return A data frame with one column for time and three columns per simulation containing coordinates in speciation cube. The data frame is saved as a CSV file in outpath.
+#' @export
 
-# Function to extract trajectory information
-extract_trajectories <- function(dir, is.complete) {
+# Function to extract trajectories from a raw data folder into a single data frame
+extract_trajectories <- function(datapath, outpath) {
 
   homedir <- getwd()
-  setwd(".")
+  setwd(datapath)
 
-  # Focus on the relevant folders, if specified
-  allFolders <- list.files()
-  if(exists("is.complete")) {
-    whichComplete <- as.logical(is.complete$is.complete)
-    whichFolders <- allFolders[whichComplete]
-  } else {
-    whichFolders <- allFolders
-  }
+  message("Extracting speciation cube trajectories...")
 
-  # Loop through these folders and record the final value of
-  trajectories <- lapply(whichFolders, function(curr.folder) {
+  # Loop through every folder in here
+  simulfolders <- list.files()
+  trajectories <- lapply(simulfolders, function(currfolder) {
 
-    message(curr.folder)
+    message(currfolder)
 
-    # Load the dataset
-    currFiles <- list.files(curr.folder)
-    if(length(grep("*.dat", currFiles)) == 0) stop(paste("data file missing from folder", curr.folder))
-    dataFile <- paste(curr.folder, currFiles[grep("*.dat", currFiles)], sep = '/')
-    simulation <- read.delim(dataFile)
+    # Go into that folder
+    setwd(currfolder)
 
-    # Return relevant columns
-    burnin <- simulation[,1] < 0 # remove burnin
-    relevantData <- simulation[!burnin,]
-    relevantData <- relevantData[,c("speciation.cube.spatial.isolation", "speciation.cube.ecological.isolation", "speciation.cube.mating.isolation")]
+    # Look for a .dat file
+    simulfiles <- list.files()
+    id.datafile <- grep(".dat$", simulfiles)
 
-    return(relevantData)
+    # Exception handling
+    if(length(id.datafile) == 0) stop(paste("data file not found in folder", currfolder))
+    if(length(id.datafile) > 1) stop(paste("more than one data file found in folder", currfolder))
+
+    # If found, read that file
+    datafile <- simulfiles[id.datafile]
+    data <- read.delim(datafile)
+
+    # Extract the time column
+    time <- data[,1]
+
+    # Pull out the cube coordinates
+    coordinates <- data[,grep("speciation.cube", colnames(data))]
+
+    setwd("..")
+
+    out <- as.data.frame(cbind(time, coordinates))
+    return(out)
 
   })
 
+  # Record the duration of each simulation
+  durations <- sapply(trajectories, nrow)
+
+  # What is the maximum duration?
+  maxduration <- max(durations)
+
+  # Are all simulations complete?
+  noextinct <- all(durations == maxduration)
+
+  # If anyone went extinct...
+  if(!noextinct) {
+
+    # Which ones?
+    id.extinct <- durations != maxduration
+
+    # For each of these simulations
+    for(id in which(id.extinct)) {
+
+      # Count the number of missing rows
+      nmissingrows <- maxduration - durations[id]
+
+      # Append a NA matrix of the right size to the existing data frame
+      trajectories[[id]] <- rbind(trajectories[[id]], matrix(NA, ncol = 4, nrow = nmissingrows))
+
+    }
+
+    # Check
+    if(!all(sapply(trajectories, nrow) == maxduration)) stop("some populations went extinct and were not properly resized with NAs")
+
+  }
+
+  # Merge everything into a data frame
+  out <- do.call("cbind", trajectories)
+
+  # Remove the time columns except the first one
+  timecols <- seq(1, ncol(out), 4)[-1]
+  out <- out[,-timecols]
+
+  # Rename columns
+  nsimul <- (ncol(out)-1)/3
+  colnames(out)[-1] <- paste0(rep(c("x", "y", "z"), nsimul), rep(seq_len(nsimul), each = 3))
+
+  # Go home
   setwd(homedir)
 
-  return(trajectories)
+  # Save output
+  write.csv(out, paste(outpath, "trajectories.csv", sep = "/"), row.names = F)
+
+  return(out)
 
 }
