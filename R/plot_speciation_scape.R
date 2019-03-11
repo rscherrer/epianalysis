000297647e47deb8b5baccd2183e8480b13f7469
R. Scherrer <p278834@WORKSPACE.RUG.NL>
2019-03-11 15:38:19 +0100

    new, more general and simpler function to plot heatmap of speciation probabilities

---
+++ R/plot_speciation_scape.R
@@ -0,0 +1,41 @@
+#' Speciation scape
+#'
+#' This is a function to visualize the speciation probability landscape in a heatmap across parameter space.
+#'
+#' @param speciation_cube_data A data frame containing trajectories in speciation cube for all simulations (in rows).
+#' @param paramspace The names of the two parameters to plot against.
+#' @param dim The variable to use as a criterion for complete speciation.
+#' @param threshold The threshold above which the value of the criterion variable means complete speciation
+#' @param n How many of the last time points to average?
+
+# Function to plot speciation probability across parameter space
+plot_speciation_scape <- function(speciation_cube_data, paramspace, dim, threshold, n = 1) {
+
+  if(length(paramspace) != 2) stop("I need two parameters to make a heatmap across parameter space")
+
+  library(ggplot2)
+
+  # Make a vector of yes/no for speciation completion according to the specified criterion
+  speciation_columns <- colnames(speciation_cube_data)[grep(dim, colnames(speciation_cube_data))]
+  speciation_columns <- speciation_columns[(length(speciation_columns) - n + 1):length(speciation_columns)]
+  speciation_criterion <- rowMeans(cbind(speciation_cube_data[, speciation_columns]))
+  is_speciation <- speciation_criterion > threshold
+
+  # Count the proportion of successful speciation events for each parameter combination
+  parameter_sets <- factor(apply(speciation_cube_data[, paramspace], 1, paste, collapse = "_"))
+  prob_speciation <- tapply(is_speciation, parameter_sets, function(outcomes_curr_set) mean(as.numeric(outcomes_curr_set)))
+  parameter_space <- do.call("rbind", lapply(strsplit(names(prob_speciation), "_"), as.numeric))
+  colnames(parameter_space) <- paste0("parameter_", seq_along(paramspace))
+
+  # Make a summary table for plotting
+  speciation_prob_data <- data.frame(parameter_space, prob_speciation)
+
+  # Plot the resulting table
+  p <- ggplot(data = speciation_prob_data, aes(x = parameter_1, y = parameter_2)) +
+    geom_tile(aes(fill = prob_speciation)) +
+    xlab(paramspace[1]) +
+    ylab(paramspace[2])
+
+  print(p)
+
+}
