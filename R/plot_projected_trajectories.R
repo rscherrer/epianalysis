8a23037d5d5961dd5dd0ac75bf81ba8825f8046b
R. Scherrer <p278834@WORKSPACE.RUG.NL>
2019-03-27 14:16:07 +0100

    can now plot confidence interval

--- R/plot_projected_trajectories.R
+++ R/plot_projected_trajectories.R
@@ -7,11 +7,12 @@
 #' @param colvar Optional. The name of the variable to use to set colors according to.
 #' @param show_legend Optional. Whether to show the legend.
 #' @param xlim,ylim Ranges of values to plot.
+#' @param confint Confidence interval probabilities. If specified, the confidence interval is plotted instead of every single line.
 #' @export
 
 
 # Function to plot trajectories in 2D
-plot_projected_trajectories <- function(speciation_cube_data, vars, colvar, show_legend = T, xlim, ylim) {
+plot_projected_trajectories <- function(speciation_cube_data, vars, colvar, show_legend = T, xlim, ylim, confint) {
 
   if(!missing(colvar)) {
 
@@ -51,37 +52,74 @@ plot_projected_trajectories <- function(speciation_cube_data, vars, colvar, show
 
   if(missing(ylim)) ylim <- c(min(coordinates_per_variable[[2]]), max(coordinates_per_variable[[2]]))
 
-  # Plot the lines
-  for(i in seq_len(nrow(speciation_cube_data))) {
+  if(!missing(confint)) {
 
-    if(i == 1) {
+    # If a confidence interval of the Y-variable is to be plotted
+    # Calculate the average trajectory
+    # Calculate the confidence interval
+    mean_y <- rowMeans(coordinates_per_variable[[2]])
+    confidence_interval_y <- t(apply(coordinates_per_variable[[2]], 1, quantile, probs = confint))
 
-      plot(
-        x = coordinates_per_variable[[1]][,i],
-        y = coordinates_per_variable[[2]][,i],
-        type = "l",
-        xlab = vars[1],
-        ylab = vars[2],
-        las = 1,
-        xlim = xlim,
-        ylim = ylim,
-        col = as.character(color_labels[i])
-      )
+    plot(
+      x = coordinates_per_variable[[1]][,1],
+      y = mean_y,
+      type = "n",
+      xlab = vars[1],
+      ylab = vars[2],
+      las = 1,
+      xlim = xlim,
+      ylim = ylim
+    )
 
-    } else {
+    poly_x <- c(coordinates_per_variable[[1]][,1], rev(coordinates_per_variable[[1]][,1]))
+    poly_y <- c(confidence_interval_y[,1], rev(confidence_interval_y[,2]))
+    polygon(
+      x = poly_x,
+      y = poly_y,
+      col = "lightgray",
+      border = NA
+    )
 
-      lines(
-        x = coordinates_per_variable[[1]][,i],
-        y = coordinates_per_variable[[2]][,i],
-        col = as.character(color_labels[i])
-      )
+    lines(
+      x = coordinates_per_variable[[1]][,1],
+      y = mean_y,
+    )
+
+  } else {
+
+    # Plot the lines
+    for(i in seq_len(nrow(speciation_cube_data))) {
+
+      if(i == 1) {
+
+        plot(
+          x = coordinates_per_variable[[1]][,i],
+          y = coordinates_per_variable[[2]][,i],
+          type = "l",
+          xlab = vars[1],
+          ylab = vars[2],
+          las = 1,
+          xlim = xlim,
+          ylim = ylim,
+          col = as.character(color_labels[i])
+        )
+
+      } else {
+
+        lines(
+          x = coordinates_per_variable[[1]][,i],
+          y = coordinates_per_variable[[2]][,i],
+          col = as.character(color_labels[i])
+        )
+
+      }
+    }
+
+    if(!missing(colvar) & show_legend) {
+
+      legend("bottomright", legend = levels(as.factor(speciation_cube_data[, colvar])), col = colors, pch = 16, title = colvar)
 
     }
-  }
-
-  if(!missing(colvar) & show_legend) {
-
-    legend("bottomright", legend = levels(as.factor(speciation_cube_data[, colvar])), col = colors, pch = 16, title = colvar)
 
   }
 
